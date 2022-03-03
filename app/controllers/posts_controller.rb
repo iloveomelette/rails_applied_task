79bcec4105e4073d8fdf032406912aa4369f4b32
iloveomelette <mizuki1998hosei@gmail.com>
2022-03-03 11:51:37 +0900

    ページネーションを実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
+  PER_PAGES = 10
+
   def index
-    @posts = Post.order(created_at: :desc)
+    @posts = Post.order(created_at: :desc).page(params[:page]).per(PER_PAGES)
   end
 
   def create
